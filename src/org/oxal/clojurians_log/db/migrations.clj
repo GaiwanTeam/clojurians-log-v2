51ca83c1407f9c9cb0d4cd17a799c17b12ee98b3
Mitesh Shah <mitesh@miteshshah.com>
2021-06-10 20:25:27 +0530

    schema and migrations

---
+++ src/org/oxal/clojurians_log/db/migrations.clj
@@ -0,0 +1,29 @@
+(ns org.oxal.clojurians-log.db.migrations
+  (:require [migratus.core :as migratus]))
+
+
+(def config {:store                :database
+             :migration-dir        "migrations/"
+             :init-script          "init.sql" ;script should be located in the :migration-dir path
+             ;defaults to true, some databases do not support
+             ;schema initialization in a transaction
+             :init-in-transaction? false
+             :migration-table-name "foo_bar"
+             :db {:classname   "org.h2.Driver"
+                  :subprotocol "h2"
+                  :subname     "site.db"}})
+
+;initialize the database using the 'init.sql' script
+(migratus/init config)
+
+;apply pending migrations
+(migratus/migrate config)
+
+;rollback the migration with the latest timestamp
+(migratus/rollback config)
+
+;bring up migrations matching the ids
+(migratus/up config 20111206154000)
+
+;bring down migrations matching the ids
+(migratus/down config 20111206154000)
---
+++ src/org/oxal/clojurians_log/db/schema.clj
@@ -0,0 +1,2 @@
+(ns org.oxal.clojurians-log.db.schema
+  (:require [next.jdbc :as jdbc]))
