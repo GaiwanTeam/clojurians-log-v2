4493e8d4ee9c0a0ed961af3af83b296b0ea2074f
Mitesh Shah <mitesh@miteshshah.com>
2021-07-05 01:18:03 +0530

    change port to 8000

--- src/clojurians_log/system.clj
+++ src/clojurians_log/system.clj
@@ -3,11 +3,12 @@
             [integrant.repl :as ig-repl]))
 
 (defn get-config []
-  {:clojurians-log.http/server {:port 8080}
+  {:clojurians-log.http/server {:port 8000}
+   :clojurians-log.http/css {}
    :clojurians-log.db.core/datasource {:dbtype "postgres"
-                                                :user "myuser"
-                                                :password "mypass"
-                                                :dbname "clojurians_log"}})
+                                       :user "myuser"
+                                       :password "mypass"
+                                       :dbname "clojurians_log"}})
 
 (ig-repl/set-prep! #(doto (get-config) ig/load-namespaces))
 
