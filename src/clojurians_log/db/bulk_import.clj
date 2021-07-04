55ea281d586eb325ed9c8c02682e3762faa9e372
Mitesh Shah <mitesh@miteshshah.com>
2021-07-04 22:51:21 +0530

    Add bulk message importer with channel cache

--- src/clojurians_log/db/bulk_import.clj
+++ src/clojurians_log/db/bulk_import.clj
@@ -1,13 +1,10 @@
 (ns clojurians-log.db.bulk-import
-  (:require [clojure.data.json :as json]
-            [clojure.java.io :as io]
+  (:require [clojure.java.io :as io]
+            [clojurians-log.db.import :as import]
             [clojurians-log.utils :as utils]
             [honey.sql :as sql]
-            [clojurians-log.utils :as utils]
-            [clojure.walk :as w]
-            [next.jdbc :as jdbc]
             [integrant.repl.state :as ig-state]
-            [clojure.set :as set]))
+            [next.jdbc :as jdbc]))
 
 (def ds (:clojurians-log.db.core/datasource ig-state/system))
 
@@ -87,3 +84,58 @@
         sqlquery (sql/format sqlmap)
         _ (println sqlquery)]
     (jdbc/execute! ds sqlquery)))
+
+
+(defn messages
+  "Imports messages idempotently based on the slack_id"
+  [ds channel cache]
+  (let [channel-dir (io/file (str "src/sample_data/" channel))
+        msg-files (file-seq channel-dir)]
+    (doseq [msg-file msg-files]
+      (when (.isFile msg-file)
+        (println "Importing from file:" (.getAbsolutePath msg-file))
+        (let [data (utils/read-json-from-file msg-file)
+              data (into []
+                         (comp
+                          (map #(utils/select-keys-nested-as
+                                 % [:text
+                                    :type
+                                    :subtype
+                                    :purpose
+                                    :user
+                                    :ts
+                                    :thread-ts]))
+                          (map #(assoc %
+                                       :channel-id
+                                       (get-in cache [:chan-name->id channel]))))
+                         data)
+              sqlvals (remove nil? (mapv #(import/event->tx % cache) data))
+              sqlmap {:insert-into [:message]
+                      :values sqlvals
+                      ;;:on-conflict []
+                      :on-conflict {:on-constraint :message_channel_id_ts_key}
+                      :do-update-set {:fields [:text :channel-id]}
+                      ;;:do-nothing true
+                      }
+              ]
+          (when (seq sqlvals)
+            (jdbc/execute! ds (sql/format sqlmap))))))))
+
+(defn chan-cache [ds]
+  (let [sqlmap {:select [:id :name]
+                :from [:channel]}
+        data (jdbc/execute! ds (sql/format sqlmap))]
+    (into {}
+          (map (juxt :channel/name :channel/id))
+          data)))
+
+(comment
+  (defn get-cache []
+    {:chan-name->id (chan-cache ds)
+     :user-slack->db-id {}})
+
+  (do
+    (channels ds)
+    (messages ds "general" (get-cache)))
+
+ ,)
