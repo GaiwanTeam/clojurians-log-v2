ee289401211ac74920c6c3dd726d1bc7033d8162
Mitesh Shah <mitesh@miteshshah.com>
2021-08-12 14:57:23 +0530

    add secrets.edn minimal support

--- src/clojurians_log/system.clj
+++ src/clojurians_log/system.clj
@@ -1,6 +1,14 @@
 (ns clojurians-log.system
   (:require [integrant.core :as ig]
-            [integrant.repl :as ig-repl]))
+            [integrant.repl :as ig-repl]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn]))
+
+(defn secrets []
+  (-> "secrets.edn"
+      io/resource
+      slurp
+      edn/read-string))
 
 (defn get-config []
   {:clojurians-log.http/server {:port 8000
