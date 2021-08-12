3ef83c8677d3a7897b8f5b43204c2e02137e7696
Mitesh Shah <mitesh@miteshshah.com>
2021-08-12 14:59:48 +0530

    clean up and stub routes

--- src/clojurians_log/routes.clj
+++ src/clojurians_log/routes.clj
@@ -4,27 +4,19 @@
             [clojurians-log.db.queries :as queries]
             [lambdaisland.ornament :as o]))
 
-(o/defstyled block :div
-  :border
-  :bg-red-100
-  :w-16
-  :h-4)
-
-(defn test-handler [request]
-  {:status 200
-   :body "Hello test!!!"})
-
 (defn handler [{:keys [ds] :as request}]
   (let [msgs (queries/all-messages ds)]
     {:status 200
-     :view (comp layout/base
-                 (fn [data]
-                   [:div
-                    #_[block [:a {:href "#"} "Lol"]]
-                    [home/section msgs]
-                    ]))
-     :body {:e 123}}))
+     :view (fn [data]
+             [layout/base
+              [home/section data]])
+     :body {:channels (queries/all-channels ds)
+            :messages msgs}}))
 
 (defn routes []
   [["/" {:get handler}]
-   ["/ww/" {:get test-handler}]])
+   ["/about" {:get handler}]
+   ["/sitemap" {:get handler}]
+   ["/healthcheck" {:get handler}]
+   ["/:channel" {:get handler}]
+   ["/:channel/:date" {:get handler}]])
