87dfa6f36718a75665e944228784a2164ea47bea
Mitesh Shah <mitesh@miteshshah.com>
2021-10-31 03:30:43 +0530

    include member data in search query messages

--- src/clojurians_log/db/queries.clj
+++ src/clojurians_log/db/queries.clj
@@ -59,9 +59,10 @@
           (map (juxt :slack-id :name))
           data)))
 
-(defn search-query [ds search-query]
-  (let [sqlmap {:select [:*]
+(defn search-messages [ds search-query]
+  (let [sqlmap {:select [:message.* :member.*]
                 :from [:message]
+                :join [:member [:= :message.member-id :member.id]]
                 :where [[:raw ["to_tsvector('english', text) @@ websearch_to_tsquery('english'," [:param :search-query] ")"]]]}
         query (sql/format sqlmap {:params {:search-query search-query}})
         data (jdbc/execute! ds query)]
