6441980778e6c7ab65bd3397cb1216085c7462f8
Mitesh Shah <mitesh@miteshshah.com>
2021-10-31 03:11:56 +0530

    add message search index

--- index.sql
+++ index.sql
@@ -1,5 +1,8 @@
-DROP index IF EXISTS message_channel_id_idx;
+DROP INDEX IF EXISTS message_channel_id_idx;
 CREATE INDEX message_channel_id_idx ON message (channel_id);
 
-DROP index IF EXISTS message_channel_id_created_at_idx;
+DROP INDEX IF EXISTS message_channel_id_created_at_idx;
 CREATE INDEX message_channel_id_created_at_idx ON message (channel_id, created_at);
+
+DROP INDEX IF EXISTS message_search_idx;
+CREATE INDEX message_search_idx ON message USING GIN (to_tsvector('english', text));
