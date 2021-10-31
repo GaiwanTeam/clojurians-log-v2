b08dfaad93991b4e460b96c79441e6180bf32d9f
Mitesh Shah <mitesh@miteshshah.com>
2021-10-31 21:14:13 +0530

    search queries sort by most recent

--- src/clojurians_log/db/queries.clj
+++ src/clojurians_log/db/queries.clj
@@ -69,7 +69,7 @@
   (let [sqlmap {:select [:message.* :member.*]
                 :from [:message]
                 :join [:member [:= :message.member-id :member.id]]
-                :order-by [[:date :desc]]
+                :order-by [[:created-at :desc]]
                 :where [[:raw ["to_tsvector('english', text) @@ websearch_to_tsquery('english'," [:param :search-query] ")"]]]}
         query (sql/format sqlmap {:params {:search-query search-query}})
         data (jdbc/execute! ds query)]
