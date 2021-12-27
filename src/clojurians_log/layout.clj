a8a039585648e0a07063ee5c674acad905e1281f
Mitesh Shah <mitesh@miteshshah.com>
2021-12-27 15:44:07 +0530

    allow injecting of extra head items

--- src/clojurians_log/layout.clj
+++ src/clojurians_log/layout.clj
@@ -2,21 +2,24 @@
   (:require [lambdaisland.ornament :as o]
             [clojure.java.io :as io]))
 
-(defn base [body]
-  [:html
-   [:head
-    [:title "Clojurians log v2"]
-    [:meta {:charset "UTF-8"}]
-    [:meta {:content "width=device-width, initial-scale=1" :name "viewport"}]
-    [:script {:data-goatcounter "https://clojurians-log-v2.goatcounter.com/count"
-              :async true
-              :src "//gc.zgo.at/count.js"}]
-    (when (io/resource "public/css/compiled/style.css")
-      [:link {:rel "stylesheet" :href "/assets/css/compiled/style.css"}])
-    (if (io/resource "public/css/compiled/ornament.css")
-      [:link {:rel "stylesheet" :href "/assets/css/compiled/ornament.css"}]
-      [:style {:type "text/css" :id "ornament"} (o/defined-styles)])]
-   [:body
-    [:div#app
-     body]
-    [:script {:type "application/javascript" :src (str "/assets/js/main.js")}]]])
+(defn base
+  ([body]
+   (base nil body))
+  ([extra-head body]
+   [:html
+    [:head
+     [:meta {:charset "UTF-8"}]
+     [:meta {:content "width=device-width, initial-scale=1" :name "viewport"}]
+     [:script {:data-goatcounter "https://clojurians-log-v2.goatcounter.com/count"
+               :async true
+               :src "//gc.zgo.at/count.js"}]
+     (when (io/resource "public/css/compiled/style.css")
+       [:link {:rel "stylesheet" :href "/assets/css/compiled/style.css"}])
+     (if (io/resource "public/css/compiled/ornament.css")
+       [:link {:rel "stylesheet" :href "/assets/css/compiled/ornament.css"}]
+       [:style {:type "text/css" :id "ornament"} (o/defined-styles)])
+     extra-head]
+    [:body
+     [:div#app
+      body]
+     [:script {:type "application/javascript" :src (str "/assets/js/main.js")}]]]))
