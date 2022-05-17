2a371fff22f9f1b9a5eab19d2184828163763963
Mitesh Shah <mitesh@miteshshah.com>
2022-05-17 17:45:40 +0530

    tx-log appender

--- src/clojurians_log/slack/socket.clj
+++ src/clojurians_log/slack/socket.clj
@@ -1,19 +1,18 @@
 (ns clojurians-log.slack.socket
   "Slack Socket App which maintains a websocket connection to Slack
   and receives real time events."
-  (:require [camel-snake-kebab.core :as csk]
-            [camel-snake-kebab.extras :as cske]
-            [clojure.edn :as edn]
-            [clojure.java.data :as jd]
-            [integrant.core :as ig])
-  (:import (com.slack.api.bolt App AppConfig)
-           (com.slack.api.bolt.handler BoltEventHandler)
-           (com.slack.api.bolt.socket_mode SocketModeApp)
-           (com.slack.api.model.event
-            ChannelCreatedEvent ChannelRenameEvent
-            MessageEvent MessageChangedEvent MessageDeletedEvent MessageRepliedEvent
-            MessageChannelJoinEvent
-            ReactionAddedEvent ReactionRemovedEvent)))
+  (:require
+   [camel-snake-kebab.core :as csk]
+   [camel-snake-kebab.extras :as cske]
+   [clojure.core.async :as async]
+   [clojure.java.data :as jd]
+   [clojure.java.io :as io]
+   [integrant.core :as ig])
+  (:import
+   (com.slack.api.bolt App AppConfig)
+   (com.slack.api.bolt.handler BoltEventHandler)
+   (com.slack.api.bolt.socket_mode SocketModeApp)
+   (com.slack.api.model.event ChannelCreatedEvent ChannelRenameEvent MessageChangedEvent MessageChannelJoinEvent MessageDeletedEvent MessageEvent MessageRepliedEvent ReactionAddedEvent ReactionRemovedEvent)))
 
 (set! *warn-on-reflection* true)
 
@@ -27,13 +26,15 @@
 
 (defn create-handler
   "Create a handler with dispatch-evt"
-  [ds]
+  [ds event-ch]
   (let [handle-event* (partial handle-event ds)]
     (reify BoltEventHandler
       (apply [_ payload ctx]
         (let [event (jd/from-java-deep (.getEvent payload) {})
               #_internal-event #_(evt-format/socket->internal m)]
           #_(tap> m)
+          (async/go
+            (async/>! event-ch event))
           (handle-event* event))
         (.ack ctx)))))
 
@@ -43,9 +44,9 @@
      (.threadPoolSize 1)
      (.singleTeamBotToken slack-bot-token))))
 
-(defn create-socket-app [{:keys [ds slack-bot-token slack-app-token]}]
+(defn create-socket-app [{:keys [ds slack-bot-token slack-app-token]} event-ch]
   (let [app-conf (create-app-conf slack-bot-token)
-        handler (create-handler ds)
+        handler (create-handler ds event-ch)
         app (doto (App. app-conf)
               (.event MessageEvent handler)
               (.event MessageChangedEvent handler)
@@ -59,14 +60,29 @@
     (doto (SocketModeApp. ^String slack-app-token app)
       (.startAsync))))
 
+(defn create-event-tx-logger [config event-ch]
+  (async/go-loop []
+    (when-let [event (async/<! event-ch)]
+      (spit "/tmp/test.txt" (prn-str event) :append true)
+      (recur))))
+
 (defmethod ig/init-key ::app [_ config]
   ;; (log/info :socket-app/starting :now)
-  (create-socket-app config))
+  (let [event-ch (async/chan 100)]
+    {:socket (create-socket-app config event-ch)
+     :tx-log (create-event-tx-logger config event-ch)
+     :event-ch event-ch}))
 
-(defmethod ig/halt-key! ::app [_ ^SocketModeApp socket-app]
+(defmethod ig/halt-key! ::app [_ {:keys [^SocketModeApp socket-app event-ch]}]
   ;; (log/info :socket-app/stoping :now)
-  (.stop ^SocketModeApp socket-app))
+  (.stop ^SocketModeApp socket-app)
+  (async/close! event-ch))
 
 (comment
-
+  (def c (async/chan 10))
+  (def tx-logger (create-event-tx-logger {} c))
+  (time
+   (doseq [i (range 30)]
+     (async/>!! c {:data (range (* i 20))})))
+  (async/close! c)
   ,)
